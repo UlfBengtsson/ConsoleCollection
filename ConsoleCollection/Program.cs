61d68277a8d4c59b31937342e3c9ad8f451e8be0
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-14 10:25:59 +0200

    Add project files.

---
+++ ConsoleCollection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleCollection
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PrimitivNumber();
+            
+            //Arrays();
+            
+        }
+
+        static void PrimitivNumber()
+        {
+            double number = 0.0;
+
+            Console.WriteLine(number);
+
+            countUpNumber(number);
+
+            Console.WriteLine("after method call");
+            Console.WriteLine(number);
+        }
+
+        static void countUpNumber(double num)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                num++;
+            }
+        }
+
+        //--------------------------------------------------------------------
+
+        static void Arrays()
+        {
+            int[,] multiTable = new int[10, 10];
+
+            foreach (var item in multiTable)
+            {
+                Console.WriteLine(item);
+            }
+
+            SetMultiNumbers(multiTable);
+
+            Console.WriteLine("after method call");
+
+            foreach (var item in multiTable)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        static void SetMultiNumbers(int[,] table)
+        {
+            for (int y = 0 ; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    table[y, x] = (y + 1) * (x + 1);
+                }
+            }
+        }
+    }
+}
